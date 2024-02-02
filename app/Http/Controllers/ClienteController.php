27fa6de3ea719cbccd406806ff12b00a658749bd
boafrancisco <bicael.francisco@gmail.com>
2024-02-02 23:00:35 +0200

    Artigo de Tarefas

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClienteController extends Controller
+{
+    public function listar(){
+        return "<h1>Clientes</h1>";
+    }
+}
